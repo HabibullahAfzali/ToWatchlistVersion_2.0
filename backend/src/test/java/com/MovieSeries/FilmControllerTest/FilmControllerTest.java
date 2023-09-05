1a14bf0b258ec75c39800c941804b23d4658e859
HabibullahAfzali <hbafzali@gmail.com>
2023-09-05 15:02:20 +0200

    method find by id

--- backend/src/test/java/com/MovieSeries/FilmControllerTest/FilmControllerTest.java
+++ backend/src/test/java/com/MovieSeries/FilmControllerTest/FilmControllerTest.java
@@ -1,5 +1,4 @@
 package com.MovieSeries.FilmControllerTest;
-
 import com.MovieSeries.controller.FilmController;
 import com.MovieSeries.model.Film;
 import com.MovieSeries.services.FilmService;
@@ -14,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -51,6 +51,19 @@ private  FilmController filmController;
 	}
 
 	@Test
+	public void testFindAll() throws Exception {
+		Film film1 = new Film();
+		film1.setId(1L);
+		Film film2 = new Film();
+		film2.setId(2L);
+		when(filmService.findAll()).thenReturn(List.of(film1, film2));
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/films"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.length()").value(2));
+	}
+
+	@Test
 	public void testGetFilmById() throws Exception {
 		Film film = new Film();
 		film.setId(1L);
@@ -63,5 +76,23 @@ private  FilmController filmController;
 
 		System.out.println("Test passed: Film found by ID");
 	}
+	@Test
+	public void testUpdateFilmById() throws Exception {
+		Film film = new Film();
+		film.setId(1L);
+		when(filmService.updateFilmById(eq(1L), any())).thenReturn(film);
 
+		mockMvc.perform(MockMvcRequestBuilders.put("/films/1")
+						.content("{}")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").value(1));
+	}
+	@Test
+	public void testDeleteById() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.delete("/films/1"))
+				.andExpect(status().isOk());
+
+		verify(filmService, times(1)).deleteById(1L);
+	}
 }
